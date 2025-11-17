87d21b489a10d20a08e2ca21ab1d679b819a892f
lRen1505 <renso.escuadra@usil.pe>
2025-11-17 18:47:41 -0500

    Añadi el constructor de ClienteObserver

--- src/app/pedidos/ClienteObserver.java
+++ src/app/pedidos/ClienteObserver.java
@@ -3,5 +3,8 @@ package app.pedidos;
 
 
 public class ClienteObserver implements Observer{
-    
+    private String nombre;
+    public ClienteObserver(String nombre) {
+        this.nombre = nombre;
+    }
 }
