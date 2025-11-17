2c321274472b474735ff27b3082ea6d899d7e5c8
lRen1505 <renso.escuadra@usil.pe>
2025-11-17 18:45:19 -0500

    Aqui cree la gestion de la lista de observadores y las notificaciones

--- src/app/pedidos/PedidoObservable.java
+++ src/app/pedidos/PedidoObservable.java
@@ -1,7 +1,34 @@
 
 package app.pedidos;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class PedidoObservable {
-    
+
+public class PedidoObservable implements Observable {
+    private List<Observer> observadores;
+    public PedidoObservable() {
+        this.observadores = new ArrayList<>();
+    }
+    @Override
+    public synchronized void agregarObservador(Observer observador) {
+        observadores.add(observador);
+        System.out.println("Observador agregado: " + observador.getClass().getSimpleName());
+    }
+    @Override
+    public synchronized void eliminarObservador(Observer observador) {
+        observadores.remove(observador);
+        System.out.println("Observador eliminado: " + observador.getClass().getSimpleName());
+    }
+    @Override
+    public synchronized void notificarObservadores(String evento, String datos) {
+        System.out.println("\nNotificando evento: " + evento);
+        System.out.println("Observadores a notificar: " + observadores.size());
+        
+        for (Observer observador : observadores) {
+            observador.actualizar(evento, datos);
+        }
+    } 
 }
+    
+
