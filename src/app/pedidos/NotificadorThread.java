f59e4c68f596406826c0a7e9972feaa434802276
lRen1505 <renso.escuadra@usil.pe>
2025-11-17 20:33:44 -0500

    Hice un while para que tome notificaciones de la cola ,envie notificaciones y confirme si estan enviadas

--- src/app/pedidos/NotificadorThread.java
+++ src/app/pedidos/NotificadorThread.java
@@ -14,4 +14,32 @@ public class NotificadorThread extends Thread{
         this.notificacionesPendientes = new LinkedBlockingQueue<>();
         this.setName("Thread-Notificador");
     }
+    @Override
+    public void run() {
+        System.out.println("\n [" + getName() + "] Iniciado - Esperando notificaciones...\n");
+        
+        while (activo) {
+            try {
+                // Toma una notificación de la cola
+                String mensaje = notificacionesPendientes.take();
+                
+                System.out.println("\n [" + getName() + "] Enviando notificación...");
+                
+                // Simular tiempo de envío (email, SMS, etc.)
+                Thread.sleep(500);
+                
+                // Enviar notificación (simulado)
+                System.out.println("Email enviado");
+                System.out.println("SMS enviado");
+                System.out.println("Push notification enviada");
+                System.out.println("Mensaje: " + mensaje);
+                
+                System.out.println("\n [" + getName() + "] Notificaciones enviadas\n");
+                
+            } catch (InterruptedException e) {
+                System.out.println("\n[" + getName() + "] Interrumpido");
+                activo = false;
+            }
+        }
+    }
 }
