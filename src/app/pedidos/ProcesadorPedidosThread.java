1c8d5d0c663b069a41325799beab13b086ce5820
lRen1505 <renso.escuadra@usil.pe>
2025-11-17 19:16:09 -0500

    Añadi la biblioteca BlockingQueue

--- src/app/pedidos/ProcesadorPedidosThread.java
+++ src/app/pedidos/ProcesadorPedidosThread.java
@@ -1,7 +1,12 @@
 
 package app.pedidos;
 
+import java.util.concurrent.BlockingQueue;
 
-public class ProcesadorPedidosThread {
-    
+
+public class ProcesadorPedidosThread extends Thread{
+    private PedidoFacade pedidoFacade;
+    private PedidoObservable observable;
+    private BlockingQueue<DatosPedido> pedidosPendientes;
+    private volatile boolean activo = true; 
 }
