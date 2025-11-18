3934af97918323480e23ed04f7ddf914885a40ed
lRen1505 <renso.escuadra@usil.pe>
2025-11-17 19:08:33 -0500

    Se registra todos los eventos en el log del sistema

--- src/app/pedidos/LogObserver.java
+++ src/app/pedidos/LogObserver.java
@@ -11,6 +11,10 @@ public class LogObserver implements Observer{
     @Override
     public void actualizar(String evento, String datos) {
       String timestamp = LocalDateTime.now().format(formatter);
+      System.out.println("Evento registrado");
+      System.out.println("Timestamp: " + timestamp);
+      System.out.println("Tipo: " + evento);
+      System.out.println("Datos: " + datos);
 
     }
 }
