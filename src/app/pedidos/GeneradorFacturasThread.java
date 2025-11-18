2b9cfd12bd23631f7b380c08d60a39e32aaa3905
lRen1505 <renso.escuadra@usil.pe>
2025-11-17 19:39:21 -0500

    Creamos las variables y la biblioteca correspondiente

--- src/app/pedidos/GeneradorFacturasThread.java
+++ src/app/pedidos/GeneradorFacturasThread.java
@@ -1,7 +1,12 @@
 
 package app.pedidos;
 
+import java.util.concurrent.BlockingQueue;
+
 
 public class GeneradorFacturasThread extends Thread {
-    
+    private FacturaService facturaService;
+    private PedidoObservable observable;
+    private BlockingQueue<DatosFactura> facturasPendientes;
+    private volatile boolean activo = true;
 }
