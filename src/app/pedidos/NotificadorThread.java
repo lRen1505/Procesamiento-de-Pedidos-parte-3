9721c7531dcdb84b5644cd5a36ab4728ee85f401
lRen1505 <renso.escuadra@usil.pe>
2025-11-17 20:29:47 -0500

    Cree las variables de NotificadorThread

--- src/app/pedidos/NotificadorThread.java
+++ src/app/pedidos/NotificadorThread.java
@@ -1,7 +1,11 @@
 
 package app.pedidos;
 
+import java.util.concurrent.BlockingQueue;
 
-public class NotificadorThread {
-    
+
+public class NotificadorThread extends Thread{
+    private PedidoObservable observable;
+    private BlockingQueue<String> notificacionesPendientes;
+    private volatile boolean activo = true;
 }
