38759d670bf7327ec9740059b83af5f0981f01e6
lRen1505 <renso.escuadra@usil.pe>
2025-11-17 19:13:43 -0500

    Inserto el constructor

--- src/app/pedidos/DatosFactura.java
+++ src/app/pedidos/DatosFactura.java
@@ -5,4 +5,10 @@ package app.pedidos;
 public class DatosFactura {
     private String cliente;
     private double total;
+  
+    public DatosFactura(String cliente, double total) {
+        this.cliente = cliente;
+        this.total = total;
+    }
+
 }
