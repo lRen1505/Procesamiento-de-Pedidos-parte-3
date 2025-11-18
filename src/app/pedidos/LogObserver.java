82ccf78c4f8d1c59b7c0df9352fff86c40089397
lRen1505 <renso.escuadra@usil.pe>
2025-11-17 19:06:15 -0500

    Añadi la biblioteca dateTimeFormatter y su metodo actualizar

--- src/app/pedidos/LogObserver.java
+++ src/app/pedidos/LogObserver.java
@@ -1,7 +1,14 @@
 
 package app.pedidos;
 
+import java.time.format.DateTimeFormatter;
 
-public class LogObserver {
-    
+
+public class LogObserver implements Observer{
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+    @Override
+    public void actualizar(String evento, String datos) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
