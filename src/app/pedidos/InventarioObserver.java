fbca2a539350e351b0783915ca08b0c440bf2715
lRen1505 <renso.escuadra@usil.pe>
2025-11-17 19:04:05 -0500

    Se crea el evento switch que actualiza el invetario cuando se procesa un pedido

--- src/app/pedidos/InventarioObserver.java
+++ src/app/pedidos/InventarioObserver.java
@@ -6,4 +6,24 @@ public class InventarioObserver implements Observer {
     @Override
     public void actualizar(String evento, String datos) {
         System.out.println("Notificación recibida");
+        switch (evento) {
+            case "PEDIDO_CREADO":
+                System.out.println(" Reservando stock para el pedido");
+                System.out.println(" Datos: " + datos);
+                // Aquí se actualizaría el stock en una BD real
+                break;
+                
+            case "PEDIDO_PROCESADO":
+                System.out.println("Descontando productos del inventario");
+                System.out.println("  " + datos);
+                break;
+                
+            case "FACTURA_GENERADA":
+                System.out.println(" Confirmando movimiento de inventario");
+                break;
+                
+            default:
+                System.out.println(" Evento no procesado por inventario: " + evento);
+        }
+    }
 }
