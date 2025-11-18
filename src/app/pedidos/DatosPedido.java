5b84ee15c475427ff78899bd4ba2538a51121810
lRen1505 <renso.escuadra@usil.pe>
2025-11-17 19:11:24 -0500

    Inserté el constructor

--- src/app/pedidos/DatosPedido.java
+++ src/app/pedidos/DatosPedido.java
@@ -6,4 +6,10 @@ public class DatosPedido {
     private String cliente;
     private String producto;
     private int cantidad;
+    
+    public DatosPedido(String cliente, String producto, int cantidad) {
+        this.cliente = cliente;
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
 }
