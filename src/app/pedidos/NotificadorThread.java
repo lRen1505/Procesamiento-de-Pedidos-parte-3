e1fb26964f5afb44615325ed81447f4b59cf3a3e
lRen1505 <renso.escuadra@usil.pe>
2025-11-17 20:30:37 -0500

    Cree el constructor y la biblioteca respectiva

--- src/app/pedidos/NotificadorThread.java
+++ src/app/pedidos/NotificadorThread.java
@@ -2,10 +2,16 @@
 package app.pedidos;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 
 public class NotificadorThread extends Thread{
     private PedidoObservable observable;
     private BlockingQueue<String> notificacionesPendientes;
     private volatile boolean activo = true;
+    public NotificadorThread(PedidoObservable observable) {
+        this.observable = observable;
+        this.notificacionesPendientes = new LinkedBlockingQueue<>();
+        this.setName("Thread-Notificador");
+    }
 }
