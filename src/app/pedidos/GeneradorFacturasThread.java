808ca1e34917f8d45d6f9356d9579c95ebafeeaf
lRen1505 <renso.escuadra@usil.pe>
2025-11-17 19:41:12 -0500

    Cree el costructor del generadorfacturas thread

--- src/app/pedidos/GeneradorFacturasThread.java
+++ src/app/pedidos/GeneradorFacturasThread.java
@@ -2,6 +2,7 @@
 package app.pedidos;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 
 public class GeneradorFacturasThread extends Thread {
@@ -9,4 +10,10 @@ public class GeneradorFacturasThread extends Thread {
     private PedidoObservable observable;
     private BlockingQueue<DatosFactura> facturasPendientes;
     private volatile boolean activo = true;
+    public GeneradorFacturasThread(FacturaService facturaService, PedidoObservable observable) {
+        this.facturaService = facturaService;
+        this.observable = observable;
+        this.facturasPendientes = new LinkedBlockingQueue<>();
+        this.setName("Thread-GeneradorFacturas");
+    }
 }
